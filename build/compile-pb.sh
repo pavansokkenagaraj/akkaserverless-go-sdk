bb9bbdb2b1fe57e59e687662a81662a5678b748c
Pavan Sokke Nagaraj <pavan.nagaraj@lightbend.com>
2021-08-18 20:47:36 -0400

    Update compile-pb.sh

--- build/compile-pb.sh
+++ build/compile-pb.sh
@@ -4,219 +4,82 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-protoc --go-grpc_out=paths=source_relative:cloudstate/protocol --proto_path=protobuf/protocol/cloudstate entity.proto
-protoc --go_out=paths=source_relative:cloudstate/protocol --proto_path=protobuf/protocol/cloudstate entity.proto
-protoc --go-grpc_out=paths=source_relative:. --proto_path=protobuf/frontend/ cloudstate/entity_key.proto
-protoc --go_out=paths=source_relative:. --proto_path=protobuf/frontend/ cloudstate/entity_key.proto
-protoc --go-grpc_out=paths=source_relative:. --proto_path=protobuf/frontend/ cloudstate/legacy_entity_key.proto
-protoc --go_out=paths=source_relative:. --proto_path=protobuf/frontend/ cloudstate/legacy_entity_key.proto
-protoc --go-grpc_out=paths=source_relative:. --proto_path=protobuf/frontend/ cloudstate/eventing.proto
-protoc --go_out=paths=source_relative:. --proto_path=protobuf/frontend/ cloudstate/eventing.proto
+# protoc akkaserverless-proxy-protocol
+protoc --go_out=paths=source_relative:akkaserverless/protocol \
+  --go-grpc_out=paths=source_relative:akkaserverless/protocol \
+  --proto_path=proto/akkaserverless/protocol discovery.proto \
+  --proto_path=proto/akkaserverless/component component.proto
 
-protoc --go-grpc_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol --proto_path=protobuf/protocol/cloudstate crdt.proto
-protoc --go_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol --proto_path=protobuf/protocol/cloudstate crdt.proto
-protoc --go-grpc_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol/ --proto_path=protobuf/protocol/cloudstate event_sourced.proto
-protoc --go_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol/ --proto_path=protobuf/protocol/cloudstate event_sourced.proto
-protoc --go-grpc_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol/ --proto_path=protobuf/protocol/cloudstate action.proto
-protoc --go_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol/ --proto_path=protobuf/protocol/cloudstate action.proto
-protoc --go-grpc_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol/ --proto_path=protobuf/protocol/cloudstate value_entity.proto
-protoc --go_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol/ --proto_path=protobuf/protocol/cloudstate value_entity.proto
+protoc --go_out=paths=source_relative:akkaserverless/action \
+  --go-grpc_out=paths=source_relative:akkaserverless/action \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/component/action action.proto
 
-protoc --go-grpc_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol/ --proto_path=protobuf/protocol/cloudstate value_entity.proto
-protoc --go_out=paths=source_relative:cloudstate/entity --proto_path=protobuf/protocol/ --proto_path=protobuf/protocol/cloudstate value_entity.proto
+protoc --go_out=paths=source_relative:akkaserverless/protocol \
+  --go-grpc_out=paths=source_relative:akkaserverless/protocol \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/component/entity entity.proto
 
-# TCK CRDT
-protoc --go-grpc_out=paths=source_relative:./tck/crdt \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck tck_crdt.proto
-protoc --go_out=paths=source_relative:./tck/crdt \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck tck_crdt.proto
+protoc --go_out=paths=source_relative:akkaserverless/entity \
+  --go-grpc_out=paths=source_relative:akkaserverless/entity \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/component/eventsourcedentity event_sourced_entity.proto
 
-# TCK CRDT2
-protoc --go-grpc_out=paths=source_relative:./tck/crdt2 \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck tck_crdt2.proto
-protoc --go_out=paths=source_relative:./tck/crdt2 \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck tck_crdt2.proto
+protoc --go_out=paths=source_relative:akkaserverless/entity \
+  --go-grpc_out=paths=source_relative:akkaserverless/entity \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/component/replicatedentity replicated_entity.proto
 
-# TCK Eventsourced
-protoc --go-grpc_out=paths=source_relative:./tck/eventsourced \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck/cloudstate/tck/model \
-  --proto_path=protobuf/tck eventsourced.proto
-protoc --go_out=paths=source_relative:./tck/eventsourced \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck/cloudstate/tck/model \
-  --proto_path=protobuf/tck eventsourced.proto
+protoc --go_out=paths=source_relative:akkaserverless/entity \
+  --go-grpc_out=paths=source_relative:akkaserverless/entity \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/component/valueentity value_entity.proto
 
-# TCK Eventlog Eventing
-protoc --go-grpc_out=paths=source_relative:./tck/eventlogeventing \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck/cloudstate/tck/model \
-  --proto_path=protobuf/tck eventlogeventing.proto
-protoc --go_out=paths=source_relative:./tck/eventlogeventing \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck/cloudstate/tck/model \
-  --proto_path=protobuf/tck eventlogeventing.proto
+protoc --go_out=paths=source_relative:akkaserverless/view \
+  --go-grpc_out=paths=source_relative:akkaserverless/view \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/component/view view.proto
 
-# TCK Action
-protoc --go-grpc_out=paths=source_relative:./tck/action \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck/cloudstate/tck/model \
-  --proto_path=protobuf/tck tck_action.proto
-protoc --go_out=paths=source_relative:./tck/action \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck/cloudstate/tck/model \
-  --proto_path=protobuf/tck tck_action.proto
+# protoc akkaserverless-sdk-protocol
+protoc --go_out=paths=source_relative:akkaserverless \
+  --go-grpc_out=paths=source_relative:akkaserverless \
+  --proto_path=proto/akkaserverless component.proto \
+  --proto_path=proto/akkaserverless eventing.proto \
+  --proto_path=proto/akkaserverless legacy_entity_key.proto \
+  --proto_path=proto/akkaserverless views.proto
 
-# TCK Value Entity
-protoc --go-grpc_out=paths=source_relative:./tck/value \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck/cloudstate/tck/model \
-  --proto_path=protobuf/tck tck_valueentity.proto
-protoc --go_out=paths=source_relative:./tck/value \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=protobuf/tck/cloudstate/tck/model \
-  --proto_path=protobuf/tck tck_valueentity.proto
+protoc --go_out=paths=source_relative:akkaserverless \
+  --go-grpc_out=paths=source_relative:akkaserverless \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless annotations.proto
 
-# CRDT shopping cart example
-protoc --go-grpc_out=paths=source_relative:./example/crdt_shoppingcart/shoppingcart \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/crdt_shoppingcart/shoppingcart shoppingcart.proto hotitems.proto
-protoc --go_out=paths=source_relative:./example/crdt_shoppingcart/shoppingcart \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/crdt_shoppingcart/shoppingcart shoppingcart.proto hotitems.proto
-protoc --go-grpc_out=paths=source_relative:./example/crdt_shoppingcart/domain \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/crdt_shoppingcart/domain domain.proto
-protoc --go_out=paths=source_relative:./example/crdt_shoppingcart/domain \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/crdt_shoppingcart/domain domain.proto
+# protoc akkaserverless-tck-protocol
+protoc --go_out=paths=source_relative:tck/action \
+  --go-grpc_out=paths=source_relative:tck/action \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/tck/model/action action.proto
 
-# Event Sourced shopping cart example
-protoc --go-grpc_out=paths=source_relative:./example/shoppingcart/ \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/shoppingcart shoppingcart.proto
-protoc --go_out=paths=source_relative:./example/shoppingcart/ \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/shoppingcart shoppingcart.proto
-protoc --go-grpc_out=paths=source_relative:./example/shoppingcart/persistence \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/shoppingcart/persistence domain.proto
-protoc --go_out=paths=source_relative:./example/shoppingcart/persistence \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/shoppingcart/persistence domain.proto
+protoc --go_out=paths=source_relative:tck/eventing \
+  --go-grpc_out=paths=source_relative:tck/eventing \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/tck/model/eventing local_persistence_eventing.proto
 
-# Value Entity shopping cart example
-protoc --go-grpc_out=paths=source_relative:./example/valueentity/ \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/valueentity value_shoppingcart.proto
-protoc --go_out=paths=source_relative:./example/valueentity/ \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/valueentity value_shoppingcart.proto
-protoc --go-grpc_out=paths=source_relative:./example/valueentity/persistence \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/valueentity/persistence value_domain.proto
-protoc --go_out=paths=source_relative:./example/valueentity/persistence \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/valueentity/persistence value_domain.proto
+protoc --go_out=paths=source_relative:tck/eventsourcedentity \
+  --go-grpc_out=paths=source_relative:tck/eventsourcedentity \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/tck/model/eventsourcedentity event_sourced_entity.proto
 
-# chat example
-protoc --go-grpc_out=paths=source_relative:./example/chat/presence/ \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/chat/presence/ example/chat/presence/presence.proto
-protoc --go_out=paths=source_relative:./example/chat/presence/ \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/chat/presence/ example/chat/presence/presence.proto
-protoc --go-grpc_out=paths=source_relative:./example/chat/friends/ \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/chat/friends/ example/chat/friends/friends.proto
-protoc --go_out=paths=source_relative:./example/chat/friends/ \
-  --proto_path=protobuf/protocol \
-  --proto_path=protobuf/frontend \
-  --proto_path=protobuf/frontend/cloudstate \
-  --proto_path=protobuf/proxy \
-  --proto_path=example/chat/friends/ example/chat/friends/friends.proto
+protoc --go_out=paths=source_relative:tck/replicatedentity \
+  --go-grpc_out=paths=source_relative:tck/replicatedentity \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/tck/model/replicatedentity replicated_entity.proto
+
+protoc --go_out=paths=source_relative:tck/valueentity \
+  --go-grpc_out=paths=source_relative:tck/valueentity \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/tck/model/valueentity value_entity.proto
+
+protoc --go_out=paths=source_relative:tck/view \
+  --go-grpc_out=paths=source_relative:tck/view \
+  --proto_path=proto \
+  --proto_path=proto/akkaserverless/tck/model/view view.proto
